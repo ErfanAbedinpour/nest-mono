476b1776f93fffa7d5b265869813414f662d9e69
erfan <erfan.wtf44@gmail.com>
2025-10-17 18:03:15 -0400

    write some methods in users.servic

--- apps/users/src/domain/entities/user.ts
+++ apps/users/src/domain/entities/user.ts
@@ -1,14 +1,35 @@
-export class User{
-    private _id: number;
-    private _username:string
-    private _password:string
+export class User {
+  constructor(
+    private readonly _id: number,
+    private readonly _username: string,
+    private readonly _password: string,
+  ) {}
 
+  static create(data: {
+    id?: number;
+    username: string;
+    password: string;
+  }): User {
+    return new User(data.id ?? Math.random(), data.username, data.password);
+  }
 
-    get id() {
-        return this._id;
+  get id() {
+    return this._id;
+  }
+
+  get username() {
+    return this._username;
+  }
+
+  static validateUsername(username: string): void {
+    if (username.length < 3) {
+      throw new Error('Username must be at least 3 characters long');
     }
+  }
 
-    get username(){
-        return this._username
+  static validatePassword(password: string): void {
+    if (password.length < 8) {
+      throw new Error('Password must be at least 8 characters long');
     }
+  }
 }
